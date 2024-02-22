74f49874da8d26d795c658d30574ee545b265c45
Łukasz Rutkowski <lukasz.rutkowski@polcode.net>
2024-02-22 09:29:45 +0100

    Add ability to create a UnitTypeEntity using a constructor

--- src/entities/Settings/UnitTypeEntity.php
+++ src/entities/Settings/UnitTypeEntity.php
@@ -1,10 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Vertuoza\Entities\Settings;
 
 class UnitTypeEntity
 {
-  public string $id;
-  public string $name;
-  public bool $isSystem;
+    public function __construct(
+        public string $id = '',
+        public string $name = '',
+        public bool $isSystem = false,
+    ) {
+    }
 }

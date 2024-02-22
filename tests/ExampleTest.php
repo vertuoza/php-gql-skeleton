8c24314d7c2c7d3f17a605042a638a93326cf4c6
Łukasz Rutkowski <lukasz.rutkowski@polcode.net>
2024-02-22 10:12:40 +0100

    Example test to prevent Phpunit failures as the result of no tests suites.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vertuoza\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}

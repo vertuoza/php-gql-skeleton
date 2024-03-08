453785ba3279e74deade538349f8803530faa54f
Dvalnir <47464326+Dvalnir@users.noreply.github.com>
2024-03-08 14:49:35 +0100

    Cleaning UnitType

--- src/graphql/Resolvers/Settings/UnitTypes/UnitType.php
+++ src/graphql/Resolvers/Settings/UnitTypes/UnitType.php
@@ -12,18 +12,18 @@ class UnitType extends ObjectType
   {
     parent::__construct([
       'name' => 'UnitType',
-      'description' => 'Unit type"',
+      'description' => 'Unit type',
       'fields' => static fn (): array => [
         'id' => [
-          'description' => "Unique identifier of the unit type",
-          'type' => Types::id(),
+          'description' => 'Unique identifier of the unit type',
+          'type' => Types::id()
         ],
         'name' => [
-          'description' => "Name of the unit type",
+          'description' => 'Name of the unit type',
           'type' => Types::string()
         ],
         'isSystem' => [
-          'description' => "To know if the unit type has been created by the user or is a system unit type of Vertuoza",
+          'description' => 'To know if the unit type has been created by the user or is a system unit type of Vertuoza',
           'type' => new NonNull(Types::boolean())
         ],
       ],
